5bf83415ef686bd31553d2268eee319310db9592
chasecurtis <chasencurtis@gmail.com>
2025-07-28 22:09:10 -0700

    add custom and built in validators for school api III and student serializer for school api IV

--- student_app/models.py
+++ student_app/models.py
@@ -1,16 +1,34 @@
 from django.db import models
+from django.utils import timezone
+from django.core import validators as v
+from .validators import (
+    validate_name,
+    validate_school_email,
+    validate_combination_format,
+)
 
 
 # Create your models here.
 class Student(models.Model):
-    name = models.CharField(max_length=255, blank=False, null=False)
-    student_email = models.EmailField(unique=True, blank=False, null=False)
+    name = models.CharField(
+        max_length=255, blank=False, null=False, validators=[validate_name]
+    )
+    student_email = models.EmailField(
+        unique=True, blank=False, null=False, validators=[validate_school_email]
+    )
     personal_email = models.EmailField(unique=True, blank=True, null=True)
     locker_number = models.IntegerField(
-        unique=True, blank=False, null=False, default=110
+        unique=True,
+        blank=False,
+        null=False,
+        default=110,
+        validators=[v.MinValueValidator(1), v.MaxValueValidator(200)],
     )
     locker_combination = models.CharField(
-        blank=False, null=False, max_length=8, default="12-12-12"
+        blank=False,
+        null=False,
+        default="12-12-12",
+        validators=[validate_combination_format],
     )
     good_student = models.BooleanField(blank=False, null=False, default=True)
 
---
+++ student_app/validators.py
@@ -0,0 +1,43 @@
+from django.core.exceptions import ValidationError
+import re
+
+
+def validate_name(name):
+    error_message = 'Name must be in the format "First Middle Initial. Last"'
+
+    regex = r"^[A-Z][a-z]+ [A-Z]\. [A-Z][a-z]+$"
+
+    good_name = re.match(regex, name)
+
+    if good_name:
+        return name
+    else:
+        raise ValidationError(error_message, params={"name": name})
+
+
+def validate_school_email(email):
+    error_message = (
+        'Invalid school email format. Please use an email ending with "@school.com".'
+    )
+
+    regex = r"^[A-Za-z0-9._%+-]+@school\.com$"
+
+    good_email = re.match(regex, email)
+
+    if good_email:
+        return email
+    else:
+        raise ValidationError(error_message, params={"email": email})
+
+
+def validate_combination_format(combination):
+    error_message = 'Combination must be in the format "12-12-12"'
+
+    regex = r"^\d{2}-\d{2}-\d{2}$"
+
+    good_combination = re.match(regex, combination)
+
+    if good_combination:
+        return combination
+    else:
+        raise ValidationError(error_message, params={"combination": combination})
