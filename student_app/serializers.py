8acca8efd607e5cb097febbce5ab13d6a4854e10
chasecurtis <chasencurtis@gmail.com>
2025-07-30 14:24:26 -0700

    Finished API VI, modified serializers, added url pathing for endpoints, added views for get requests for students and subjects

--- student_app/serializers.py
+++ student_app/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Student
+from subject_app.serializers import SubjectSerializer
 
 
 class StudentSerializer(serializers.ModelSerializer):
@@ -9,6 +10,16 @@ class StudentSerializer(serializers.ModelSerializer):
 
 
 class StudentAllSerializer(serializers.ModelSerializer):
+    subjects = SubjectSerializer(many=True)
+
     class Meta:
         model = Student
-        exclude = ["id"]
+        fields = [
+            "name",
+            "student_email",
+            "personal_email",
+            "locker_number",
+            "locker_combination",
+            "good_student",
+            "subjects",
+        ]
--- subject_app/views.py
+++ subject_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Subject
+from .serializers import SubjectSerializer
 
 # Create your views here.
+
+
+class All_subjects(APIView):
+    def get(self, request):
+        subjects = Subject.objects.all()
+        serialized_subjects = SubjectSerializer(subjects, many=True)
+        return Response(serialized_subjects.data)
